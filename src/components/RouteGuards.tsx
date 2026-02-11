fa8870daa2457abe5744fd736ad1063cbcee2549
Dew <eidnwq@gmail.com>
2026-02-11 21:44:14 +0900

    refactor: PrivateRoute 내 불필요한 alert 제거

--- src/components/RouteGuards.tsx
+++ src/components/RouteGuards.tsx
@@ -1,16 +1,9 @@
 import { useAuthStore } from "@/stores/useAuthStore";
-import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 export const PrivateRoute = () => {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   const hasHydrated = useAuthStore((state) => state.hasHydrated);
-
-  useEffect(() => {
-    if (hasHydrated && !isAuthenticated) {
-      alert("로그인이 필요한 서비스입니다.");
-    }
-  }, [hasHydrated, isAuthenticated]);
 
   if (!hasHydrated) {
     return null;
