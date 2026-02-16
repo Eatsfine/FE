f1cda0f60aedde4f6bb9ecbc8975107207228b57
JAESEON PARK <ddorri83@naver.com>
2026-02-16 22:49:19 +0900

    feat: RestaurantListSkeleton 추가

---
+++ src/components/restaurant/RestaurantCardSkeleton.tsx
@@ -0,0 +1,3 @@
+export default function RestaurantCardSkeleton() {
+  return <div>리스트카드 스켈레톤</div>;
+}
---
+++ src/components/restaurant/RestaurantListSkeleton.tsx
@@ -0,0 +1,18 @@
+import RestaurantCardSkeleton from "./RestaurantCardSkeleton";
+
+export default function RestaurantListSkeleton({
+  count = 8,
+}: {
+  count?: number;
+}) {
+  return (
+    <div className="border rounded-xl bg-white overflow-hidden">
+      {Array.from({ length: count }).map((_, idx) => (
+        <div key={idx}>
+          <RestaurantCardSkeleton />
+          {idx !== count - 1 ? <div className="h-px bg-gray-100" /> : null}
+        </div>
+      ))}
+    </div>
+  );
+}
